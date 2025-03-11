5697cc0e846d216643f4aad05c57c125ef53bbc6
AndrewZ-0 <107640838+AndrewZ-0@users.noreply.github.com>
2025-03-11 21:47:13 +0000

    Add files via upload

--- PhimoLive/intergrators/euler.js
+++ PhimoLive/intergrators/euler.js
@@ -1,15 +1,16 @@
-
+//explicit
 export function euler_updateParticles(computeForces, applyCollisions, particles, planes, dt) {
     computeForces(particles, planes);
-    applyCollisions(particles, planes);
 
     for (const p of particles) {
-        p.v[0] += p.a[0] * dt;
-        p.v[1] += p.a[1] * dt;
-        p.v[2] += p.a[2] * dt;
-
         p.s[0] += p.v[0] * dt;
         p.s[1] += p.v[1] * dt;
         p.s[2] += p.v[2] * dt;
+
+        p.v[0] += p.a[0] * dt;
+        p.v[1] += p.a[1] * dt;
+        p.v[2] += p.a[2] * dt;
     }
+
+    applyCollisions(particles, planes);
 }
